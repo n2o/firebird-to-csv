430ddf285f914cb825cb03200775a672855f11a6
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-11-14 23:51:19 +0100

    Add seed for database

--- hotpie.py
+++ hotpie.py
@@ -2,8 +2,22 @@ import fdb
 
 host = "localhost"
 port = "3050"
-database = "/home/n2o/Projects/firebird-to-xml/database/hotpie.fdb"
+database = "/home/firebird/foo.fdb"
 user = "sysdba"
 password = "masterkey"
 
+
+def add_seed(cur):
+    seed = [
+        (0, "12345", "Herr", "", "Christian", "Meter", "", "Foostr. 42", "4711", "Baztown", "", "", "Deutschland", "0123 45678", "", "foo@bar.com", "http://www.bar.com")
+    ]
+    cur.executemany("insert into adresse (ID, KUNDENNR, ANREDE, KURZNAME, NAME1, NAME2, NAME3, STRASSE, PLZ1, ORT, PLZ2, PFACH, LAND, TELEFON, TELEFAX, EMAIL, HOMEPAGE) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", seed)
+
+
 con = fdb.connect(host=host, port=port, database=database, user=user, password=password) 
+
+# Create Object that operates in the context of con
+cur = con.cursor()
+
+add_seed(cur)
+con.commit()
