aad7e6861ac5718b0da0f62e017b1e623e3da3f1
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-11-19 01:13:49 +0100

    Remove java stuff

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,9 +1,12 @@
-(defproject hotpie "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject hotpie "0.1"
+  :description "Query Firebird database and convert the results to a CSV file."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [hikari-cp "1.7.5"]
+                 [org.clojure/java.jdbc "0.7.0-alpha1"]
+                 [org.firebirdsql.jdbc/jaybird "2.2.5"]]
   :main ^:skip-aot hotpie.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- clojure/src/hotpie/core.clj
+++ clojure/src/hotpie/core.clj
@@ -1,7 +1,57 @@
 (ns hotpie.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.edn :as edn]
+            [clojure.data.csv :as csv]
+            [hikari-cp.core :refer :all]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.java.io :as io])
+  (import (org.firebirdsql.pool FBSimpleDataSource)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def data
+  (edn/read-string (slurp "config.edn")))
+
+(def datasource-options
+  (let [firebird (:firebird data)]
+    {:connection-timeout 30000
+     :validation-timeout 5000
+     :idle-timeout       600000
+     :max-lifetime       1800000
+     :minimum-idle       10
+     :maximum-pool-size  10
+     :pool-name          "db-pool"
+     :adapter            "firebird"
+     :username           (:username firebird)
+     :password           (:password firebird)
+     :database-name      (:database firebird)
+     ;;:host               (:host firebird)
+     ;;:port               (:port firebird)
+     }))
+
+(def datasource
+  (make-datasource datasource-options))
+
+(defn get-rows
+  "Makes database query and returns all matching rows fitting to :query."
+  []
+  (jdbc/with-db-connection [conn {:datasource datasource}]
+    (jdbc/query conn (:query data))))
+
+(defn convert-one-row
+  "Take one row from the database and order the fields."
+  [row]
+  (map #((keyword %) row) (get-in data [:csv :order])))
+
+(defn rows->csv
+  "Given all rows from database, convert them and write them to a file."
+  [rows]
+  (let [csv-data (:csv data)]
+    (with-open [out-file (io/writer (:file-location csv-data))]
+      (csv/write-csv
+       out-file
+       (cons (get-in data [:csv :headers]) rows)
+       :separator (get-in data [:csv :separator])))))
+
+(defn -main []
+  (rows->csv (map convert-one-row (get-rows)))
+  (close-datasource datasource))
+
