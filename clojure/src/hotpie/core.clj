aed82248c7d4de94b77bd53658f7d4d9d2fedb84
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-12-18 23:52:16 +0100

    Example configuration for firebird with korma

--- clojure/src/hotpie/core.clj
+++ clojure/src/hotpie/core.clj
@@ -2,46 +2,45 @@
   (:gen-class)
   (:require [clojure.edn :as edn]
             [clojure.data.csv :as csv]
-            [hikari-cp.core :refer :all]
+            ;[hikari-cp.core :refer :all]
             [clojure.java.jdbc :as jdbc]
             [clojure.java.io :as io])
   (import (org.firebirdsql.pool FBSimpleDataSource)))
 
+(require '[korma.core :as sql])
+(use '[korma.db])
+
+(def db-config
+  (firebird {:db "/home/firebird/foo.fdb"
+             :host "localhost"
+             :port "3050"
+             :user "sysdba"
+             :password "masterkey"
+             :make-pool? false}))
+
+(defdb fdb db-config)
+
+fdb
+
+(default-connection fdb)
+
+(sql/exec-raw fdb "SELECT * FROM adresse" :results)
+
 (def data
   (edn/read-string (slurp "config.edn")))
 
-(def datasource-options
-  (let [firebird (:firebird data)]
-    {:connection-timeout 30000
-     :validation-timeout 5000
-     :idle-timeout       600000
-     :max-lifetime       1800000
-     :minimum-idle       10
-     :maximum-pool-size  10
-     :pool-name          "db-pool"
-     :adapter            "firebird"
-     :username           (:username firebird)
-     :password           (:password firebird)
-     :database-name      (:database firebird)
-     ;;:host               (:host firebird)
-     ;;:port               (:port firebird)
-     }))
-
-(def datasource
-  (make-datasource datasource-options))
-
-(defn get-rows
+#_(defn get-rows
   "Makes database query and returns all matching rows fitting to :query."
   []
   (jdbc/with-db-connection [conn {:datasource datasource}]
     (jdbc/query conn (:query data))))
 
-(defn convert-one-row
-  "Take one row from the database and order the fields."
+#_(defn convert-one-row
+  "Take one row from the database and apply the order of the fields."
   [row]
   (map #((keyword %) row) (get-in data [:csv :order])))
 
-(defn rows->csv
+#_(defn rows->csv
   "Given all rows from database, convert them and write them to a file."
   [rows]
   (let [csv-data (:csv data)]
@@ -51,7 +50,9 @@
        (cons (get-in data [:csv :headers]) rows)
        :separator (get-in data [:csv :separator])))))
 
-(defn -main []
+#_(defn -main []
   (rows->csv (map convert-one-row (get-rows)))
   (close-datasource datasource))
 
+
+#_(-main)
