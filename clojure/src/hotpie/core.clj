310432394cf5c07d835a407c3da485af7158c44b
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-12-26 20:52:17 +0100

    Rewrite app to use korma

--- clojure/src/hotpie/core.clj
+++ clojure/src/hotpie/core.clj
@@ -2,13 +2,13 @@
   (:gen-class)
   (:require [clojure.edn :as edn]
             [clojure.data.csv :as csv]
+            [clojure.string :refer [upper-case]]
             ;[hikari-cp.core :refer :all]
-            [clojure.java.jdbc :as jdbc]
-            [clojure.java.io :as io])
-  (import (org.firebirdsql.pool FBSimpleDataSource)))
-
-(require '[korma.core :as sql])
-(use '[korma.db])
+            ;[clojure.java.jdbc :as jdbc]
+            [clojure.java.io :as io]
+            [korma.core :as sql])
+  (:use [korma.db])
+  #_(import (org.firebirdsql.pool FBSimpleDataSource)))
 
 (def db-config
   (firebird {:db "/home/firebird/foo.fdb"
@@ -17,30 +17,23 @@
              :user "sysdba"
              :password "masterkey"
              :make-pool? false}))
-
 (defdb fdb db-config)
-
-fdb
-
 (default-connection fdb)
-
-(sql/exec-raw fdb "SELECT * FROM adresse" :results)
 
 (def data
   (edn/read-string (slurp "config.edn")))
 
-#_(defn get-rows
+(defn get-rows
   "Makes database query and returns all matching rows fitting to :query."
   []
-  (jdbc/with-db-connection [conn {:datasource datasource}]
-    (jdbc/query conn (:query data))))
+  (sql/exec-raw fdb (:query data) :results))
 
-#_(defn convert-one-row
+(defn convert-one-row
   "Take one row from the database and apply the order of the fields."
   [row]
-  (map #((keyword %) row) (get-in data [:csv :order])))
+  (map #((keyword (upper-case (name %))) row) (get-in data [:csv :order])))
 
-#_(defn rows->csv
+(defn rows->csv
   "Given all rows from database, convert them and write them to a file."
   [rows]
   (let [csv-data (:csv data)]
@@ -50,9 +43,7 @@ fdb
        (cons (get-in data [:csv :headers]) rows)
        :separator (get-in data [:csv :separator])))))
 
-#_(defn -main []
-  (rows->csv (map convert-one-row (get-rows)))
-  (close-datasource datasource))
+(defn -main []
+  (rows->csv (map convert-one-row (get-rows))))
 
-
-#_(-main)
+(-main)
