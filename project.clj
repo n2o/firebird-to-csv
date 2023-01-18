23f594299e3b5b749f07c783068e3358010ff5ec
Christian Meter <cmeter@googlemail.com>
2023-01-18 22:35:20 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "Query Firebird database and convert the results to a CSV file."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.csv "1.0.0"]
-                 [org.firebirdsql.jdbc/jaybird "2.2.5"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.csv "1.0.1"]
+                 [org.firebirdsql.jdbc/jaybird "5.0.0.java11"]
                  [korma "0.4.3"]]
   :main ^:skip-aot hotpie.core
   :target-path "target/%s"
